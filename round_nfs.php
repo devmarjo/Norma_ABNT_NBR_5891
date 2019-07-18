7c5814c9c45d1dd1d0d12cddd7594d3e653221d3
markinhosmasa <40844609+markinhosmasa@users.noreply.github.com>
2019-07-18 15:10:43 -0400

    Create round_nfs.php

---
+++ round_nfs.php
@@ -0,0 +1,18 @@
+
+function round_nfs($numero, $decimais){
+	$numero1 = $numero*pow(10,$decimais);
+  $numero2 = floor($numero1);
+  $numero3 = floatval(bcsub($numero1, $numero2,3));
+	if(($numero3) < 0.5){
+		return $numero2/(pow(10,$decimais));
+	}
+	elseif(($numero3) >= 0.51){
+		return ($numero2+1)/(pow(10,$decimais));
+	}else{
+		if(($numero2+1)%2==0){
+			return ($numero2+1)/(pow(10,$decimais));
+		}else{
+			return $numero2/(pow(10,$decimais));
+		}
+	}
+}
